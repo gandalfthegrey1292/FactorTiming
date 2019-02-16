7aa2a3fdcf3a247e0b9ab812aa52784acac83723
yyang <yangying9586@gmail.com>
2019-02-16 14:35:47 -0800

    new

--- step1.py
+++ step1.py
@@ -11,14 +11,25 @@ import os
 os.chdir('C:/UCB/230K/factor timing project')
 import datetime
 
-
+#
 inflation = pd.read_csv('WRDS_inflation.csv')
-
 inflation.set_index(pd.to_datetime(inflation.caldt, format = '%Y%m%d'),inplace = True)
 del inflation['caldt']
+inflation.index.names = ['date']
 
 factor = pd.read_csv('WRDS_factors.csv')
 factor.set_index(pd.to_datetime(factor.date, format = '%Y%m%d'), inplace = True)
 del factor['date']
+factor_m = factor.resample('M').sum()
+
+ret_df = pd.merge(inflation, factor_m, how = 'inner', left_index = True, right_index = True)
+
+def static_equal_weight(ret_df, assetsa):
+    weight = 1/len(assets)
+    portfolio = (weight * ret_df[assets]).sum(axis = 1)
+    return portfolio
+
+    
+
 
 
