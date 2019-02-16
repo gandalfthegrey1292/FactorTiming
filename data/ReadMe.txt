57a13d943aabb47fd241c47c66c4c41f67bfbe9d
Saurabh Kelkar <saurabh1292@gmail.com>
2019-02-16 11:18:03 -0800

    Initial Commit
    Creating Coding skeleton and Structure for the project

---
+++ data/ReadMe.txt
@@ -0,0 +1,9 @@
+###############################################################################
+Instructions to which files to place here
+###############################################################################
+
+Place only the factor files here. Preferrably all factors in a csv already.
+If multiple set of factors from sources then place multiple files.
+
+The raw set of files should be available on the google drive:
+https://drive.google.com/drive/u/1/folders/17MfqxOKpW9MCi8f9c4xP9z3f__oQYFUd
---
+++ src/main.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Main file for Factor Timing Project
+"""
+# %%
+###############################################################################
+# Imports
+###############################################################################
+import timingLibrary as lib
+
+
+# %%
+###############################################################################
+# Data
+# Make function Calls and create a factor data Frame
+# End output after block should have
+# a. DataFrame of Factors with column names as Factors
+# b. Row index as Date. All data to be business month end or business daily
+###############################################################################
+
+# %%
+###############################################################################
+# Weights Allocation
+# Make calls to each strategy function to return a dataframe of weights.
+# End Output after each function should be
+# a. Columns alined with data frame from above
+# b. Row index aligned with data frame above
+###############################################################################
+
+# %%
+###############################################################################
+# Benchmark Returns
+# returns a single column DataFrame with benchmark Returns
+###############################################################################
+
+# %%
+###############################################################################
+# Back Test Code
+# Function Call that backtests
+# NOTE: NEED TO ARTICULATE MORE
+############################################################################### 
